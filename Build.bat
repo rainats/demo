44d7f20f651f16ff8d365b1d4898e927cc91d097
rainats <43172111+rainats@users.noreply.github.com>
2021-08-17 19:20:21 +0530

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,6 +1,5 @@
 echo "Building the Project : %date% : %time%"
 mvn install
 mvn compiler:compile com.example.demo
-mvn compiler:testCompile 
 mvn jar:jar
 echo "Building the Project : Done %date% : %time%"
