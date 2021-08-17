92635dd472d1ac4a14103448d7faf7f6d4f40e54
rainats <43172111+rainats@users.noreply.github.com>
2021-08-17 18:29:27 +0530

    making changes to compile and run build

--- Build.bat
+++ Build.bat
@@ -1,1 +1,4 @@
 echo "Building the Project : %date% : %time%"
+javac com.example.demo.DemoApplication.java
+java com.example.demo.DemoApplication.java
+echo "Building the Project : Done %date% : %time%"
