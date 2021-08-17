00a3c31100caca5255ace59b3316ba166d19f8f1
rainats <43172111+rainats@users.noreply.github.com>
2021-08-17 18:51:09 +0530

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 echo "Building the Project : %date% : %time%"
-bat 'mvn -B -DskipTests clean com.example.demo'
+mvn -B -DskipTests clean com.example.demo
 javac DemoApplication.java
 java DemoApplication.java
 echo "Building the Project : Done %date% : %time%"
