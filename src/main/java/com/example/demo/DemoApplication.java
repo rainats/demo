9b09957d4c34ea1d42f4704b8cb40bd3e35ef50b
rainats <43172111+rainats@users.noreply.github.com>
2021-08-17 20:26:20 +0530

    Update DemoApplication.java
    adding imports

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.example.demo")
 @SpringBootApplication
 public class DemoApplication {
 
