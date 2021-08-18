7d2c9e1d57b7117493bb8b173ab1cc4b12b171b3
rainats <43172111+rainats@users.noreply.github.com>
2021-08-18 10:19:03 +0530

    Update Build.bat
    changed mvn install to mvn install:install for plugin goal

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 echo "Building the Project : %date% : %time%"
-mvn install
+mvn install:install
 mvn compiler:compile com.example.demo
 mvn jar:jar
 echo "Building the Project : Done %date% : %time%"
